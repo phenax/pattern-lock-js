bf8cd98c3b2550252e3e156448ae56a4171b1cb3
Akshay Nair <phenax5@gmail.com>
2018-09-15 12:57:48 +0530

    Introducers hyperapp and refactors stuff to use that insteaad of bdom

--- build/vanilla.example.js
+++ build/vanilla.example.js
@@ -1,57 +1,95 @@
 "use strict";
 
-var _PatternLock = _interopRequireDefault(require("./PatternLock"));
+var _hyperapp = require("hyperapp");
 
-var _bdom = require("./example-helpers/bdom");
+var _PatternLock = _interopRequireDefault(require("./PatternLock"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var PatternLockCanvas = function PatternLockCanvas() {
-  var $canvas = (0, _bdom.h)('canvas')();
-  var lock = (0, _PatternLock.default)({
-    $canvas: $canvas,
-    width: 300,
-    height: 430,
-    grid: [3, 3]
-  }); // Right L, Diagonal L
-
-  lock.matchHash(['LTExNjI0MjcxOTA=', 'MTQ2NjgyMjczMw==', 'LTYyMzEzNTM2Ng==']).onSuccess(function () {
-    return lock.setThemeState('success');
-  }).onFailure(function () {
-    return lock.setThemeState('failure');
-  });
-  lock.onStart(function () {
-    return lock.setThemeState('default');
-  });
-  return {
-    lock: lock,
-    $canvas: $canvas
-  };
-};
-
+// import { div, input, text, h, onChange, render } from './example-helpers/bdom';
+// const PatternLockCanvas = () => {
+// 	const $canvas = h('canvas')();
+// 	const lock = PatternLock({
+// 		$canvas,
+// 		width: 300,
+// 		height: 430,
+// 		grid: [ 3, 3 ],
+// 	});
+// 	// Right L, Diagonal L
+// 	lock.matchHash([ 'LTExNjI0MjcxOTA=', 'MTQ2NjgyMjczMw==', 'LTYyMzEzNTM2Ng==' ])
+// 		.onSuccess(() => lock.setThemeState('success'))
+// 		.onFailure(() => lock.setThemeState('failure'));
+// 	lock.onStart(() => lock.setThemeState('default'));
+// 	return { lock, $canvas };
+// };
+// const App = ({ grids, themes, themeStates }) => {
+// 	const { lock, $canvas } = PatternLockCanvas();
+// 	const state = {
+// 		grid: { value: '', index: 1 },
+// 		theme: { value: '', index: 0 },
+// 		themeState: { value: '', index: 0 },
+// 	};
+// 	const $password = input();
+// 	lock.onComplete(({ hash } = {}) => $password.value = hash);
+// 	const $codeBox = div();
+// 	const renderCodeBox = () => render(CodeExample({ ...state }), $codeBox);
+// 	renderCodeBox();
+// 	const stateChange = (stateName, action) => (value, index) => {
+// 		state[stateName] = { value, index };
+// 		renderCodeBox();
+// 		return action(value);
+// 	};
+// 	const $app = div({}, [
+// 		div({ class: 'title' }, [ text('PatternLockJS') ]),
+// 		div({ class: 'subtitle' }, [ text('Draw unlock pattern to generate a hash') ]),
+// 		div({ class: 'canvas-wrapper' }, [ $canvas ]),
+// 		div({ class: 'password' }, [ text('Your password is: '), $password ]),
+// 		div({}, [ $codeBox ]),
+// 		div({}, [
+// 			OptionsGroup({
+// 				name: 'Grid',
+// 				list: grids,
+// 				selected: state.grid.index,
+// 				onItemSelect: stateChange('grid', grid => () => lock.setGrid(...grid)),
+// 			}),
+// 			OptionsGroup({
+// 				name: 'Theme',
+// 				list: themes,
+// 				selected: state.theme.index,
+// 				onItemSelect: stateChange('theme', theme => () => lock.setTheme(theme)),
+// 			}),
+// 			OptionsGroup({
+// 				name: 'Theme State',
+// 				list: themeStates,
+// 				selected: state.themeState.index,
+// 				onItemSelect: stateChange('themeState', ts => () => lock.setThemeState(ts)),
+// 			}),
+// 		]),
+// 	]);
+// 	return { $app, lock };
+// };
 var OptionItem = function OptionItem(_ref) {
   var name = _ref.name,
       value = _ref.value,
       isSelected = _ref.isSelected,
       onSelect = _ref.onSelect;
-  return (0, _bdom.h)('label')({
+  return (0, _hyperapp.h)('label', {
     style: 'padding: .3em .5em;'
-  }, [(0, _bdom.onChange)(onSelect, (0, _bdom.input)(_defineProperty({
+  }, [(0, _hyperapp.h)('input', _objectSpread({
     type: 'radio',
-    name: name
-  }, isSelected ? 'checked' : 'unchecked', true))), (0, _bdom.text)(value)]);
+    name: name,
+    onchange: onSelect
+  }, isSelected ? {
+    checked: true
+  } : {})), value.toString()]);
 };
 
 var OptionsGroup = function OptionsGroup(_ref2) {
@@ -59,131 +97,114 @@ var OptionsGroup = function OptionsGroup(_ref2) {
       onItemSelect = _ref2.onItemSelect,
       name = _ref2.name,
       selected = _ref2.selected;
-  return (0, _bdom.div)({
+  return (0, _hyperapp.h)('div', {
     style: 'padding: 1em 0;'
-  }, [(0, _bdom.div)({
+  }, [(0, _hyperapp.h)('div', {
     style: 'font-size: 1.3em;'
-  }, [(0, _bdom.h)('strong')({}, [(0, _bdom.text)(name)])]), (0, _bdom.div)({}, list.map(function (item, index) {
+  }, (0, _hyperapp.h)('strong', {}, name)), (0, _hyperapp.h)('div', {}, list.map(function (item, index) {
     return OptionItem({
       name: name,
       value: item,
       isSelected: index === selected,
-      onSelect: onItemSelect(item, index)
+      onSelect: onItemSelect(index)
     });
   }))]);
 };
 
 var CodeExample = function CodeExample(_ref3) {
-  var grid = _ref3.grid,
-      theme = _ref3.theme,
-      themeState = _ref3.themeState;
-  return (0, _bdom.div)({}, [(0, _bdom.text)(JSON.stringify({
-    grid: grid,
-    theme: theme
-  }))]);
+  var _ref3$tabSize = _ref3.tabSize,
+      tabSize = _ref3$tabSize === void 0 ? 4 : _ref3$tabSize,
+      props = _objectWithoutProperties(_ref3, ["tabSize"]);
+
+  return (0, _hyperapp.h)('div', {
+    style: 'text-align: left; padding: .7em 1em; background-color: #eee;'
+  }, (0, _hyperapp.h)('pre', {}, JSON.stringify(props, 0, tabSize)));
 };
 
-var App = function App(_ref4) {
-  var grids = _ref4.grids,
-      themes = _ref4.themes,
-      themeStates = _ref4.themeStates;
-
-  var _PatternLockCanvas = PatternLockCanvas(),
-      lock = _PatternLockCanvas.lock,
-      $canvas = _PatternLockCanvas.$canvas;
-
-  var state = {
-    grid: {
-      value: '',
-      index: 1
+var App = {
+  state: {
+    gridIndex: 1,
+    themeIndex: 0,
+    themeStateIndex: 0,
+    count: 0
+  },
+  actions: {
+    incr: function incr() {
+      return function (_ref4) {
+        var count = _ref4.count;
+        return {
+          count: count + 1
+        };
+      };
     },
-    theme: {
-      value: '',
-      index: 0
+    setGrid: function setGrid(gridIndex) {
+      return function () {
+        return {
+          gridIndex: gridIndex
+        };
+      };
     },
-    themeState: {
-      value: '',
-      index: 0
+    setTheme: function setTheme(themeIndex) {
+      return function () {
+        return {
+          themeIndex: themeIndex
+        };
+      };
+    },
+    setThemeState: function setThemeState(themeStateIndex) {
+      return function () {
+        return {
+          themeStateIndex: themeStateIndex
+        };
+      };
     }
-  };
-  var $password = (0, _bdom.input)();
-  lock.onComplete(function () {
-    var _ref5 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
-        hash = _ref5.hash;
-
-    return $password.value = hash;
-  });
-  var $codeBox = (0, _bdom.div)();
-
-  var renderCodeBox = function renderCodeBox() {
-    return (0, _bdom.render)(CodeExample(_objectSpread({}, state)), $codeBox);
-  };
-
-  renderCodeBox();
-
-  var stateChange = function stateChange(stateName, action) {
-    return function (value, index) {
-      state[stateName] = {
-        value: value,
-        index: index
-      };
-      renderCodeBox();
-      return action(value);
+  },
+  render: function render(_ref5) {
+    var grids = _ref5.grids,
+        themes = _ref5.themes,
+        themeStates = _ref5.themeStates;
+    return function (state, actions) {
+      return (0, _hyperapp.h)('div', {}, [CodeExample({
+        state: state
+      }), (0, _hyperapp.h)('div', {}, [OptionsGroup({
+        name: 'Grid',
+        list: grids,
+        selected: state.gridIndex,
+        onItemSelect: function onItemSelect(index) {
+          return function () {
+            return actions.setGrid(index);
+          };
+        }
+      }), OptionsGroup({
+        name: 'Theme',
+        list: themes,
+        selected: state.themeIndex,
+        onItemSelect: function onItemSelect(index) {
+          return function () {
+            return actions.setTheme(index);
+          };
+        }
+      }), OptionsGroup({
+        name: 'Theme State',
+        list: themeStates,
+        selected: state.themeStateIndex,
+        onItemSelect: function onItemSelect(index) {
+          return function () {
+            return actions.setThemeState(index);
+          };
+        }
+      })]), (0, _hyperapp.h)('button', {
+        onclick: actions.incr
+      }, 'incr')]);
     };
-  };
-
-  var $app = (0, _bdom.div)({}, [(0, _bdom.div)({
-    class: 'title'
-  }, [(0, _bdom.text)('PatternLockJS')]), (0, _bdom.div)({
-    class: 'subtitle'
-  }, [(0, _bdom.text)('Draw unlock pattern to generate a hash')]), (0, _bdom.div)({
-    class: 'canvas-wrapper'
-  }, [$canvas]), (0, _bdom.div)({
-    class: 'password'
-  }, [(0, _bdom.text)('Your password is: '), $password]), (0, _bdom.div)({}, [$codeBox]), (0, _bdom.div)({}, [OptionsGroup({
-    name: 'Grid',
-    list: grids,
-    selected: state.grid.index,
-    onItemSelect: stateChange('grid', function (grid) {
-      return function () {
-        return lock.setGrid.apply(lock, _toConsumableArray(grid));
-      };
-    })
-  }), OptionsGroup({
-    name: 'Theme',
-    list: themes,
-    selected: state.theme.index,
-    onItemSelect: stateChange('theme', function (theme) {
-      return function () {
-        return lock.setTheme(theme);
-      };
-    })
-  }), OptionsGroup({
-    name: 'Theme State',
-    list: themeStates,
-    selected: state.themeState.index,
-    onItemSelect: stateChange('themeState', function (ts) {
-      return function () {
-        return lock.setThemeState(ts);
-      };
-    })
-  })])]);
-  return {
-    $app: $app,
-    lock: lock
-  };
+  }
 };
-
 document.addEventListener('DOMContentLoaded', function () {
-  var _App = App({
+  var $appRoot = document.getElementById('root');
+  var view = App.render({
     grids: [[2, 2], [3, 3], [3, 4], [4, 4], [4, 5]],
     themes: ['dark', 'light'],
     themeStates: ['default', 'success', 'failure']
-  }),
-      $app = _App.$app,
-      lock = _App.lock;
-
-  var $appRoot = document.getElementById('root');
-  (0, _bdom.render)($app, $appRoot);
-  lock.recalculateBounds();
+  });
+  (0, _hyperapp.app)(App.state, App.actions, view, $appRoot);
 });
--- src/vanilla.example.js
+++ src/vanilla.example.js
@@ -1,125 +1,162 @@
+import { h, app } from 'hyperapp';
 
 import PatternLock from './PatternLock';
-import { div, input, text, h, onChange, render } from './example-helpers/bdom';
+// import { div, input, text, h, onChange, render } from './example-helpers/bdom';
 
 
-const PatternLockCanvas = () => {
-	const $canvas = h('canvas')();
+// const PatternLockCanvas = () => {
+// 	const $canvas = h('canvas')();
 
-	const lock = PatternLock({
-		$canvas,
-		width: 300,
-		height: 430,
-		grid: [ 3, 3 ],
-	});
+// 	const lock = PatternLock({
+// 		$canvas,
+// 		width: 300,
+// 		height: 430,
+// 		grid: [ 3, 3 ],
+// 	});
 
-	// Right L, Diagonal L
-	lock.matchHash([ 'LTExNjI0MjcxOTA=', 'MTQ2NjgyMjczMw==', 'LTYyMzEzNTM2Ng==' ])
-		.onSuccess(() => lock.setThemeState('success'))
-		.onFailure(() => lock.setThemeState('failure'));
+// 	// Right L, Diagonal L
+// 	lock.matchHash([ 'LTExNjI0MjcxOTA=', 'MTQ2NjgyMjczMw==', 'LTYyMzEzNTM2Ng==' ])
+// 		.onSuccess(() => lock.setThemeState('success'))
+// 		.onFailure(() => lock.setThemeState('failure'));
 
-	lock.onStart(() => lock.setThemeState('default'));
+// 	lock.onStart(() => lock.setThemeState('default'));
 
-	return { lock, $canvas };
-};
+// 	return { lock, $canvas };
+// };
+
+// const App = ({ grids, themes, themeStates }) => {
+// 	const { lock, $canvas } = PatternLockCanvas();
+// 	const state = {
+// 		grid: { value: '', index: 1 },
+// 		theme: { value: '', index: 0 },
+// 		themeState: { value: '', index: 0 },
+// 	};
+
+// 	const $password = input();
+// 	lock.onComplete(({ hash } = {}) => $password.value = hash);
+	
+// 	const $codeBox = div();
+// 	const renderCodeBox = () => render(CodeExample({ ...state }), $codeBox);
+
+// 	renderCodeBox();
+// 	const stateChange = (stateName, action) => (value, index) => {
+// 		state[stateName] = { value, index };
+// 		renderCodeBox();
+// 		return action(value);
+// 	};
+
+// 	const $app = div({}, [
+// 		div({ class: 'title' }, [ text('PatternLockJS') ]),
+// 		div({ class: 'subtitle' }, [ text('Draw unlock pattern to generate a hash') ]),
+// 		div({ class: 'canvas-wrapper' }, [ $canvas ]),
+// 		div({ class: 'password' }, [ text('Your password is: '), $password ]),
+// 		div({}, [ $codeBox ]),
+// 		div({}, [
+// 			OptionsGroup({
+// 				name: 'Grid',
+// 				list: grids,
+// 				selected: state.grid.index,
+// 				onItemSelect: stateChange('grid', grid => () => lock.setGrid(...grid)),
+// 			}),
+// 			OptionsGroup({
+// 				name: 'Theme',
+// 				list: themes,
+// 				selected: state.theme.index,
+// 				onItemSelect: stateChange('theme', theme => () => lock.setTheme(theme)),
+// 			}),
+// 			OptionsGroup({
+// 				name: 'Theme State',
+// 				list: themeStates,
+// 				selected: state.themeState.index,
+// 				onItemSelect: stateChange('themeState', ts => () => lock.setThemeState(ts)),
+// 			}),
+// 		]),
+// 	]);
+
+// 	return { $app, lock };
+// };
 
 const OptionItem = ({ name, value, isSelected, onSelect }) => (
-	h('label')({ style: 'padding: .3em .5em;' }, [
-		onChange(
-			onSelect,
-			input({
-				type: 'radio',
-				name,
-				[isSelected ? 'checked': 'unchecked']: true
-			}),
-		),
-		text(value),
+	h('label', { style: 'padding: .3em .5em;' }, [
+		h('input', {
+			type: 'radio',
+			name,
+			onchange: onSelect,
+			...(isSelected? { checked: true }: {}),
+		}),
+		value.toString(),
 	])
 );
 
 const OptionsGroup = ({ list, onItemSelect, name, selected }) => (
-	div({ style: 'padding: 1em 0;' }, [
-		div({ style: 'font-size: 1.3em;' }, [ h('strong')({}, [ text(name) ]) ]),
-		div({},
+	h('div', { style: 'padding: 1em 0;' }, [
+		h('div', { style: 'font-size: 1.3em;' }, h('strong', {}, name)),
+		h('div', {},
 			list.map((item, index) => OptionItem({
 				name,
 				value: item,
 				isSelected: index === selected,
-				onSelect: onItemSelect(item, index),
-			}))
+				onSelect: onItemSelect(index),
+			})),
 		),
 	])
 );
 
-const CodeExample = ({ grid, theme, themeState }) => {
-	return div({}, [
-		text(JSON.stringify({
-			grid,
-			theme,
-		})),
-	]);
-};
+const CodeExample = ({ tabSize = 4, ...props }) => (
+	h('div',
+		{ style: 'text-align: left; padding: .7em 1em; background-color: #eee;' },
+		h('pre', {}, JSON.stringify(props, 0, tabSize))
+	)
+);
 
-const App = ({ grids, themes, themeStates }) => {
-	const { lock, $canvas } = PatternLockCanvas();
-	const state = {
-		grid: { value: '', index: 1 },
-		theme: { value: '', index: 0 },
-		themeState: { value: '', index: 0 },
-	};
-
-	const $password = input();
-	lock.onComplete(({ hash } = {}) => $password.value = hash);
-	
-	const $codeBox = div();
-	const renderCodeBox = () => render(CodeExample({ ...state }), $codeBox);
-
-	renderCodeBox();
-	const stateChange = (stateName, action) => (value, index) => {
-		state[stateName] = { value, index };
-		renderCodeBox();
-		return action(value);
-	};
-
-	const $app = div({}, [
-		div({ class: 'title' }, [ text('PatternLockJS') ]),
-		div({ class: 'subtitle' }, [ text('Draw unlock pattern to generate a hash') ]),
-		div({ class: 'canvas-wrapper' }, [ $canvas ]),
-		div({ class: 'password' }, [ text('Your password is: '), $password ]),
-		div({}, [ $codeBox ]),
-		div({}, [
+const App = {
+	state: {
+		gridIndex: 1,
+		themeIndex: 0,
+		themeStateIndex: 0,
+		count: 0,
+	},
+	actions: {
+		incr: () => ({ count }) => ({ count: count + 1 }),
+		setGrid: gridIndex => () => ({ gridIndex }),
+		setTheme: themeIndex => () => ({ themeIndex }),
+		setThemeState: themeStateIndex => () => ({ themeStateIndex }),
+	},
+	render: ({ grids, themes, themeStates }) => (state, actions) => h('div', {}, [
+		CodeExample({ state }),
+		h('div', {}, [
 			OptionsGroup({
 				name: 'Grid',
 				list: grids,
-				selected: state.grid.index,
-				onItemSelect: stateChange('grid', grid => () => lock.setGrid(...grid)),
+				selected: state.gridIndex,
+				onItemSelect: index => () => actions.setGrid(index),
 			}),
 			OptionsGroup({
 				name: 'Theme',
 				list: themes,
-				selected: state.theme.index,
-				onItemSelect: stateChange('theme', theme => () => lock.setTheme(theme)),
+				selected: state.themeIndex,
+				onItemSelect: index => () => actions.setTheme(index),
 			}),
 			OptionsGroup({
 				name: 'Theme State',
 				list: themeStates,
-				selected: state.themeState.index,
-				onItemSelect: stateChange('themeState', ts => () => lock.setThemeState(ts)),
+				selected: state.themeStateIndex,
+				onItemSelect: index => () => actions.setThemeState(index),
 			}),
 		]),
-	]);
-
-	return { $app, lock };
+		h('button', { onclick: actions.incr }, 'incr'),
+	]),
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-	const { $app, lock } = App({
+	const $appRoot = document.getElementById('root');
+
+	const view = App.render({
 		grids: [ [2,2], [3,3], [3, 4], [4,4], [4,5] ],
 		themes: [ 'dark', 'light' ],
 		themeStates: [ 'default', 'success', 'failure' ],
 	});
-	const $appRoot = document.getElementById('root');
-	render($app, $appRoot);
-	lock.recalculateBounds();
+
+	app(App.state, App.actions, view, $appRoot);
 });
 
